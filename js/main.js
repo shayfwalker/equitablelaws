665e878fdf0ea2a3c73ca5d403d465464bddbaef
Shayfwalker <shayfwalker@gmail.com>
2018-01-18 21:07:08 -0800

    second commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+	
+window.onload = function randomImage(){
+		var images = [
+				'./img/downtown_1.jpg',
+				'./img/downtown_7.jpg',
+				'./img/downtown_8.jpg',
+				'./img/downtown_10.jpg'
+					];
+					
+		document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] +')';		
+ 		document.body.style.backgroundRepeat = "no-repeat";
+ 		document.body.style.backgroundSize = "cover"
+						}
+					
+						
+
+document.addEventListener("DOMContentLoaded", randomImage()); 		
+
+				
