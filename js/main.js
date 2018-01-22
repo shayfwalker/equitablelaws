5569bbb2c98c69a20df9c84487af5c797a3f752e
Shayfwalker <shayfwalker@gmail.com>
2018-01-22 14:35:43 -0800

    Third commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ window.onload = function randomImage(){
 				'./img/downtown_1.jpg',
 				'./img/downtown_7.jpg',
 				'./img/downtown_8.jpg',
-				'./img/downtown_10.jpg'
+				'./img/downtown_10.jpg',
+				'./img/downtown_9.jpg'
 					];
 					
 		document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] +')';		
